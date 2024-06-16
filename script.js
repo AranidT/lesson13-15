3c3354a1fcc5ba1d5d12ed3e513fe1deb833eb51
Dinara <dinarazamatova@gmail.com>
2024-06-16 23:41:55 +0500

    introduction to js

---
+++ script.js
@@ -0,0 +1,80 @@
+//EASY
+//1
+let num = 42;
+console.log(num)
+
+//2
+let str = "Hello, world!";
+console.log(str)
+
+//3
+let isTrue = true;
+console.log(isTrue)
+
+//4
+let a = 5, b = 3;
+console.log(a + b)
+
+//5 
+const pi = 3.14;
+console.log(pi)
+
+//6
+let name = "Dinara";
+console.log(name)
+
+//7
+let age = 23;
+console.log(age)
+
+//8
+let message = "Welcone to my website";
+console.log(message)
+
+//9
+let x = 10, y = 5;
+console.log(x - y)
+
+//10
+let firstName = "Dinara", lastName = "Azamatova";
+console.log(firstName, lastName)
+
+//11
+let width = 10, height = 14;
+let S = width * height
+console.log("Площадь прямоугольника: " + S)
+
+//12
+let greeting = "Hello", name1 = "Dinara";
+console.log(greeting, name1)
+
+//13
+let radius = 5;
+console.log(pi * radius ** 2)
+
+//14
+let numl = 10, num2 = 3;
+console.log(numl / num2)
+
+//15
+let num3 = 7, num4 = 2;
+console.log(num3 % num4)
+
+//MEDIUM 
+//1
+let celsia = 20;
+
+let farengeit = (celsia * 9/5) + 32;
+let kelvin = celsia + 273.15;
+
+console.log("Температура в градусах Фаренгейта:" + farengeit + "F")
+console.log("Температура в градусах Кельвина:" + kelvin + "K")
+
+//2
+let deposit = 1000;
+let rate = 0.05;
+
+console.log("Сумма вклада через год: " + deposit * (1 + rate) ** 1)
+console.log("Сумма вклада через два года: " + deposit * (1 + rate) ** 2)
+console.log("Сумма вклада через три года: " + deposit * (1 + rate) ** 3)
+
